6f51387ca5deeaeda3a2d917fc028c0372ad8451
Miron Ogrodowicz <miron.ogrodowicz@gmail.com>
2017-08-16 21:36:39 +0200

    removed sed because it crashes the container when the gremlin-server config is mounted from host

--- run.sh
+++ run.sh
@@ -47,11 +47,6 @@ wait_for_startup Cassandra \
 	return 1
 }
 
-# use cassandra backed db instead of berkeleyje
-sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
-sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
-sed -i "s/channelizer: org.apache.tinkerpop.gremlin.server.channel.WebSocketChannelizer/channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer/g" conf/gremlin-server/gremlin-server.yaml
-
 # create the backing file
 echo "gremlin.graph=com.thinkaurelius.titan.core.TitanFactory
 storage.backend=cassandra
